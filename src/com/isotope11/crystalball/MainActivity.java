e4ce044800ecf281b20e13a028c15339db244751
Josh Adams <josh@isotope11.com>
2013-03-31 01:19:45 -0500

    Get through to shake-to-answer

--- src/com/isotope11/crystalball/MainActivity.java
+++ src/com/isotope11/crystalball/MainActivity.java
@@ -1,33 +1,88 @@
 package com.isotope11.crystalball;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.isotope11.crystalball.ShakeDetector.OnShakeListener;
 
 public class MainActivity extends Activity {
 
 	private Crystalball mCrystalball = new Crystalball();
+	private TextView mAnswerLabel;
+	private ImageView mCrystalBallImage;
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private ShakeDetector mShakeDetector;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        // Declare our view variables and assign them the views from the layout file
-        final TextView answerLabel = (TextView) findViewById(R.id.textView1);
-        Button getAnswerButton = (Button) findViewById(R.id.button1);
-            
-        getAnswerButton.setOnClickListener(new View.OnClickListener() {	
+        // assign the views from the layout file
+        mAnswerLabel = (TextView) findViewById(R.id.textView1);
+        mCrystalBallImage = (ImageView) findViewById(R.id.imageView1);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector(new OnShakeListener() {
+			
 			@Override
-			public void onClick(View v) {
-				answerLabel.setText(mCrystalball.getAnAnswer());
+			public void onShake() {
+				handleNewAnswer();
 			}
 		});
     }
+    
+    @Override
+    public void onResume(){
+    	super.onResume();
+    	mSensorManager.registerListener(mShakeDetector, mAccelerometer,
+        		SensorManager.SENSOR_DELAY_UI);
+    }
+    
+    @Override
+    public void onPause(){
+    	super.onPause();
+    	mSensorManager.unregisterListener(mShakeDetector);
+    }
+    
+    private void animateCrystalBall() {
+        mCrystalBallImage.setImageResource(R.drawable.ball_animation);
+        
+        AnimationDrawable ballAnimation = (AnimationDrawable) mCrystalBallImage.getDrawable();
+        if (ballAnimation.isRunning()){
+        	ballAnimation.stop();
+        }
+        ballAnimation.start();
+    }
+    
+    private void animateAnswer() {
+    	AlphaAnimation fadeInAnimation = new AlphaAnimation(0, 1);
+    	fadeInAnimation.setDuration(1500);
+    	fadeInAnimation.setFillAfter(true);
+    	mAnswerLabel.setAnimation(fadeInAnimation);
+    }
 
+    private void playSound() {
+    	MediaPlayer player = MediaPlayer.create(this, R.raw.crystal_ball);
+    	player.start();
+    	player.setOnCompletionListener(new OnCompletionListener() {
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				// Release player.
+				mp.release();
+			}
+		});
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +90,12 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+	private void handleNewAnswer() {
+		mAnswerLabel.setText(mCrystalball.getAnAnswer());
+		animateCrystalBall();
+		animateAnswer();
+		playSound();
+	}
     
 }
